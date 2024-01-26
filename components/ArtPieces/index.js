afea2733920514627503e977a10449bf578808aa
Niko Cremer <n.ko@posteo.de>
2024-01-26 11:16:57 +0100

    create ArtPiece component

---
+++ components/ArtPieces/index.js
@@ -0,0 +1,26 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function ArtPieces() {
+  const URL = `https://example-apis.vercel.app/api/art`;
+
+  const { data: artworks, error, isLoading } = useSWR(URL, fetcher);
+
+  if (isLoading || !artworks) return <p>is loading...</p>;
+  if (error) return <p>error</p>;
+  console.log(artworks);
+
+  return (
+    <ul>
+      {artworks.map((artwork) => {
+        return (
+          <li key={artwork.slug}>
+            {artwork.name}
+            by {artwork.artist}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
-import useSWR from "swr";
-
-const URL = `https://example-apis.vercel.app/api/art`;
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+import ArtPieces from "@/components/ArtPieces";
 
 export default function HomePage() {
-  const { data: artworks, error, isLoading } = useSWR(URL, fetcher);
-
-  if (isLoading || !artworks) return <p>is loading...</p>;
-  if (error) return <p>error</p>;
-  console.log(artworks);
-
   return (
     <div>
       <h1>Hello from Niko,Nicole and Ramin</h1>
-
-      <ul>
-        {artworks.map((artwork) => {
-          return (
-            <li key={artwork.slug}>
-              {artwork.name}
-              by {artwork.artist}
-            </li>
-          );
-        })}
-      </ul>
+      <ArtPieces />
     </div>
   );
 }
