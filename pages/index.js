219cb9a19d77657d013e2d3a852ced175879fa3f
Ramin Faqyri <ramin-faqyri@web.de>
2024-01-26 10:48:37 +0100

    fetched data from API

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import useSWR from "swr";
+
+const URL = `https://example-apis.vercel.app/api/art`;
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (isLoading || !data) return <p>is loading...</p>;
+  if (error) return <p>error</p>;
+  console.log(data);
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Hello from Niko,Nicole and Ramin</h1>
+      <p>{data[0].artist}</p>
     </div>
   );
 }
