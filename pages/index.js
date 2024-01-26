d18082b045550684c475494aa04706a68e338fc8
Niko Cremer <n.ko@posteo.de>
2024-01-26 11:04:18 +0100

    maping list of name and artists

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,26 @@ const URL = `https://example-apis.vercel.app/api/art`;
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function HomePage() {
-  const { data, error, isLoading } = useSWR(URL, fetcher);
+  const { data: artworks, error, isLoading } = useSWR(URL, fetcher);
 
-  if (isLoading || !data) return <p>is loading...</p>;
+  if (isLoading || !artworks) return <p>is loading...</p>;
   if (error) return <p>error</p>;
-  console.log(data);
+  console.log(artworks);
 
   return (
     <div>
       <h1>Hello from Niko,Nicole and Ramin</h1>
-      <p>{data[0].artist}</p>
+
+      <ul>
+        {artworks.map((artwork) => {
+          return (
+            <li key={artwork.slug}>
+              {artwork.name}
+              by {artwork.artist}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
