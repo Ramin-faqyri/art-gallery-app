ab01bd66d374555363ed94011046a0bbf0b4fe6e
Ramin Faqyri <ramin-faqyri@web.de>
2024-01-26 12:30:59 +0100

    try adding images

--- components/ArtPiecePreview/index.js
+++ components/ArtPiecePreview/index.js
@@ -1,5 +1,14 @@
 import { artworks } from "@/components/ArtPieces/index.js";
+import Image from "next";
 
 export default function ArtPiecePreview({ image, title, artist }) {
-  return <h1>{title}</h1>;
+  const imgSrc = image.src;
+  console.log(image);
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{artist}</p>
+      <Image src={imgSrc} />
+    </>
+  );
 }
