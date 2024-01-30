a81771c5e42bee584b3a0e63bae7ad3484ddcfcd
Niko Cremer <n.ko@posteo.de>
2024-01-30 12:39:30 +0100

    style detail page

--- components/ArtPieceDetails/index.js
+++ components/ArtPieceDetails/index.js
@@ -7,50 +7,57 @@ export function ArtPieceDetails({ imageSource, title, artist, year, genre }) {
   console.log("titel", title);
   return (
     <>
-      <ArtPieceDetailImage
-        src={imageSource}
-        width={300}
-        height={300}
-        //style={{ width: "300px", height: "auto" }}
-        //fill={true}
-      />
       <DetailsBox>
-        <p>
-          Title:
-          <br />
-          {title}
-          <br />
-          <br />
-          Artist:
-          <br />
-          {artist}
-        </p>
+        <ArtPieceDetailImage
+          src={imageSource}
+          width={300}
+          height={300}
+          //style={{ width: "300px", height: "auto" }}
+          //fill={true}
+        />
+        <DetailsInfoBox>
+          <p>
+            Title:
+            <br />
+            {title}
+            <br />
+            <br />
+            Artist:
+            <br />
+            {artist}
+          </p>
 
-        <p>
-          Year:
-          <br />
-          {year}
-          <br />
-          Genre:
-          <br />
-          {genre}
-        </p>
+          <p>
+            Year:
+            <br />
+            {year}
+            <br />
+            Genre:
+            <br />
+            {genre}
+          </p>
+        </DetailsInfoBox>
       </DetailsBox>
     </>
   );
 }
 
 const ArtPieceDetailImage = styled(Image)`
-  width: 300px;
+  width: 20rem;
   height: auto;
   padding: 2rem;
 `;
-
 const DetailsBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const DetailsInfoBox = styled.div`
   margin-top: 2rem;
   background-color: white;
   border-radius: 12px;
-  width: 20rem;
+  width: 300px;
   margin-bottom: 2rem;
   padding-top: 2rem;
   padding-left: 2rem;
